5e283b00641af1c28b51fe6a5a0e8a33effc8005
JavierO <javier.ortiz13@upr.edu>
2019-11-06 11:58:33 -0400

    fixed landing alignments

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import pageContent from './../../content/aboutme.json'
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public content:{name:string, code:string} = pageContent;
   public show:boolean = false;
 
   constructor() { }
