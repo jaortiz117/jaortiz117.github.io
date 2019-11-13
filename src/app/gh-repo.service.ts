c4ba6a2b6823f2e8f8395157fc1309deeb641239
JavierO <javier.ortiz13@upr.edu>
2019-11-13 00:43:41 -0400

    added gh api

--- src/app/gh-repo.service.ts
+++ src/app/gh-repo.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export interface RepoData {
   name: string;
   description: string;
-  url: string;
+  html_url: string;
 }
 
 @Injectable({
@@ -14,19 +16,26 @@ export class GhRepoService {
 
   constructor(private _http: HttpClient) { }
 
-  getAllRepoData(): RepoData[] {
-    let data = [];
-    this._http.get('https://api.github.com/users/jaortiz117/repos').subscribe(function(x){
-      data = x;
-    });
-    var result = data.map(function(datum){
-      return <RepoData>{
-        name: datum.name,
-        description: datum.description,
-        url: datum.html_url
-      };
-    });
+  getAllRepoData(): Observable<RepoData[]> {
 
-    return result;
+    return this._http.get<RepoData[]>('https://api.github.com/users/jaortiz117/repos').pipe(
+      catchError(this.handleError)
+    );
   }
+
+  private handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.error('An error occurred:', error.error.message);
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    console.error(
+      `Backend returned code ${error.status}, ` +
+      `body was: ${error.error}`);
+  }
+  // return an observable with a user-facing error message
+  return throwError(
+    'Something bad happened; please try again later.');
+};
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { GhRepoService, RepoData } from './../gh-repo.service.ts';
+import { GhRepoService, RepoData } from './../gh-repo.service';
+import { Observable } from 'rxjs';
+
 import pageContent from './../../content/repo-showcase.json'
 
 @Component({
@@ -9,27 +11,54 @@ import pageContent from './../../content/repo-showcase.json'
 })
 export class PortfolioComponent implements OnInit {
   public content:{name:string, code:string} = pageContent;
-  public repoData: RepoData[] = [];
+  public repoData: RepoData[];
+  public data: RepoData[];
 
-  constructor(private _repoService: GhRepoService) {  }
+  constructor(private _repoService: GhRepoService) {
+
+  }
 
   ngOnInit() {
-    this.repoData = this._repoService.getAllRepoData();
+
+    // this.repoData$.subscribe(function(x){
+    //   x.forEach(e => {
+    //     this.data.push(e);
+    //   })
+    // });
   }
 
-  getDescription(name: string){
-    var result = this.repoData.find(function(element){
-      return element.name === name;
-    });
+  async getData(name: string){
+    try {
+      this.repoData = await this._repoService.getAllRepoData().toPromise();
 
-    return result[0].description;
+      let result: RepoData;
+
+      this.repoData.forEach(function(x){
+        if(x.name == name){
+          result = x;
+        }
+      });
+
+      return result;
+    }
+    catch(err) {
+      return null;
+    }
+  }
+  async getDescription(name: string){
+    let result = await this.getData(name);
+    if(result){
+      return result.description;
+    }
+    return "Github API is currently Busy and can't fetch a description";
   }
 
-  getLink(name: string){
-    var result = this.repoData.find(function(element){
-      return element.name === name;
-    });
-    return result[0].url;
+  async getLink(name: string){
+    let result = await this.getData(name);
+    if(result){
+      return result.html_url;
+    }
+    return "https://github.com/jaortiz117";
   }
 
 }
